0602a42628257d3e3af4ef2f913ce5a929ad2d9f
Mike Chaberski <mchaberski@gmail.com>
2018-02-06 15:25:38 -0500

    try touching test file in target dir

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,6 +7,10 @@ set -e
 SRC_DIR=$(readlink -f $(dirname $0))
 echo "using source directory $SRC_DIR"
 ls -ld ${SRC_DIR}
+mkdir -p "${SRC_DIR}/target"
+TESTFILE="${SRC_DIR}/target/testfile"
+touch ${TESTFILE}
+echo "touched ${TESTFILE}"
 cd ${SRC_DIR}
 MVN='/usr/local/maven-3.5.2/bin/mvn'
 echo "mvn: resolving dependencies"
