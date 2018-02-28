203ce815af0fba26606c8826a3989d1276d3f617
Mike Chaberski <mchaberski@gmail.com>
2018-02-28 17:49:42 -0500

    attempt cross-platform compat in ReplayServerConfig_MappingPathTest

--- har-replay-core/src/test/java/io/github/mike10004/harreplay/ReplayServerConfig_MappingPathTest.java
+++ har-replay-core/src/test/java/io/github/mike10004/harreplay/ReplayServerConfig_MappingPathTest.java
@@ -2,6 +2,7 @@ package io.github.mike10004.harreplay;
 
 import io.github.mike10004.harreplay.ReplayServerConfig.MappingMatch;
 import io.github.mike10004.harreplay.ReplayServerConfig.StringLiteral;
+import org.apache.commons.io.FilenameUtils;
 import org.junit.Test;
 
 import java.io.File;
@@ -13,14 +14,35 @@ public class ReplayServerConfig_MappingPathTest {
 
     @Test
     public void mappingPathResolution() {
-        Path root = new File(System.getProperty("java.io.tmpdir")).toPath();
+        Path root = getRoot();
         String relativePath = "a/relative/path";
         File file = StringLiteral.of(relativePath).resolveFile(root, MOCKMATCH, MOCKURL);
         assertTrue("absolute", file.isAbsolute());
         assertEquals("path", root.resolve(relativePath), file.toPath());
-        String absolutePath = "/usr/lib/library.so";
+    }
+
+    private static Path getRoot() {
+        return new File(System.getProperty("java.io.tmpdir")).toPath();
+    }
+
+    @Test
+    public void mappingPathResolution_absolutePathSpecified() {
+        Path root = getRoot();
+        String absolutePath = constructPlatformDependentAbsolutePath("/usr/lib/library.so");
         File absfile = StringLiteral.of(absolutePath).resolveFile(root, MOCKMATCH, MOCKURL);
         assertEquals("abs", new File(absolutePath).toPath(), absfile.toPath());
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static String constructPlatformDependentAbsolutePath(String unixNormalizedAbsPath) {
+        boolean windows = org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS;
+        if (windows) {
+            File cwd = new File(System.getProperty("user.dir")).getAbsoluteFile();
+            String prefix = FilenameUtils.getPrefix(cwd.getAbsolutePath());
+            return FilenameUtils.normalizeNoEndSeparator(prefix + unixNormalizedAbsPath);
+        } else {
+            return unixNormalizedAbsPath;
+        }
     }
 
     private static final MappingMatch MOCKMATCH = url -> {
