7b3ac9bdece93d87d81297d6f51eed9d1f71247b
Mike Chaberski <mchaberski@gmail.com>
2018-07-12 12:16:38 -0400

    skip tests requiring deb file if OS is not linux

--- har-replay-dist/src/test/java/io/github/mike10004/harreplay/dist/DebIT.java
+++ har-replay-dist/src/test/java/io/github/mike10004/harreplay/dist/DebIT.java
@@ -1,5 +1,8 @@
 package io.github.mike10004.harreplay.dist;
 
+import org.apache.commons.lang3.SystemUtils;
+import org.junit.Assume;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -7,6 +10,11 @@ import java.io.File;
 import static org.junit.Assert.assertTrue;
 
 public class DebIT {
+
+    @BeforeClass
+    public static void ignoreOnNonLinuxPlatform() {
+        Assume.assumeTrue("this test is ignored if the OS is not Linux because debian-maven-plugin skips the deb-building step if the OS is not Linux", SystemUtils.IS_OS_LINUX);
+    }
 
     @Test
     public void checkDeb() throws Exception {
